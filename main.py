3745fb25a9d167fdeee20b05bc36ebfa7ee09d9c
TooCuteToo <bao.2001181020@gmail.com>
2024-09-09 20:59:01 +0700

    feature: move the player sprite to the rotate direction and group Player by using pygame group

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
@@ -16,8 +21,13 @@ def main():
                 return 
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -5,13 +5,17 @@ import constants
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, constants.PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 180
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        fwd = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += fwd * constants.PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -21,6 +25,12 @@ class Player(circleshape.CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
 
 
     def triangle(self):
